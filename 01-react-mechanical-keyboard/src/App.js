1b1e9cb249d4541f2404ef4bb0888d1cec7ba07b
joanneks <joanne.kan@trentglobal.edu.sg>
2022-07-12 08:38:20 +0000

    added homepage function

---
+++ 01-react-mechanical-keyboard/src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import React from "react";
+import HomePage from "./HomePage";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div><HomePage/></div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ 01-react-mechanical-keyboard/src/HomePage.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo1 from './logo-cropped.png';
+import background from './background.jpeg';
+import './HomePage.css';
+
+export default function Homepage() {
+  return (
+    <div className="HomePage">
+      <div className="HomePage-logo-div">
+        <img src={logo1} className="HomePage-logo" alt="logo"/>
+      </div>
+        <div className="container HomePage-content-box">
+          <div className="row">
+            <div className="col col-1"></div>
+            <div className="HomePage-inspire col col-10 HomePage-text">
+              <h1 className="HomePage-text">INSPIRE</h1>
+              <h3 className="HomePage-text">Share mechanical keyboard creations</h3>
+              <button className="btn btn-primary HomePage-text">Create</button>
+            </div>
+            <div className="col col-1"></div>
+            <div className="col col-5"></div>
+            <div className="HomePage-or col col-2">
+              <h1 className="HomePage-text">OR</h1>
+            </div>
+            <div className="col col-5"></div>
+            <div className="col col-1"></div>
+            <div className="HomePage-be-inspired col col-10 HomePage-text">
+              <h1 className="HomePage-text">BE INSPIRED</h1>
+              <h3 className="HomePage-text">Search for mechanical keyboards</h3>
+              <button className="btn btn-primary HomePage-text">Search</button>
+            </div>
+            <div className="col col-1"></div>
+          </div>
+        </div>
+    </div>
+  );
+}
+
